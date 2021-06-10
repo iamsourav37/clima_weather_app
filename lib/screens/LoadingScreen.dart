31d16f9106b95b73fca7bdb823d1f9931def9e86
iamsourav37 <souravganguly108@gmail.com>
2021-06-10 09:40:12 +0530

    get request on openweather api dummy

--- lib/screens/LoadingScreen.dart
+++ lib/screens/LoadingScreen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:clima/services/Location.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -9,17 +12,27 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  final String apiKey = "ndyZ8IV21f22nME2RMU3mQow6B3w0KZy";
+  final String apiKey = "4c15e5152554d721a2d364658112e7cf";
   late final lat;
   late final long;
   String api =
-      "http://api.accuweather.com/locations/v1/cities/geoposition/search.json?q=40.78, -77.89&apikey={your key}";
+      "api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={API key}";
+
+  String tmp =
+      "http://api.openweathermap.org/data/2.5/weather?lat=23.797138333333336&lon=87.36568333333335&appid=4c15e5152554d721a2d364658112e7cf";
 
   void fetchLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
     print(location.latitude);
     print(location.longitude);
+  }
+
+  void getData() async {
+    var response = await http.get(Uri.parse(tmp));
+    print("Response type : ${response.runtimeType}, response : $response");
+    var data = jsonDecode(response.body);
+    print("data type: ${data.runtimeType}, data : $data");
   }
 
   @override
@@ -32,7 +45,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("clima app"),
+        child: ElevatedButton(
+          onPressed: () {
+            getData();
+          },
+          child: Text("get data"),
+        ),
       ),
     );
   }
